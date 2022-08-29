f923bf524e8f146579ab7bdd56042423af35985b
zheming <455281092@qq.com>
2022-08-29 19:38:10 +0800

    hot-fix commit1

--- src/main/java/com/atchen/git/GitTest.java
+++ src/main/java/com/atchen/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello GitHub!");
         System.out.println("hello GitHub!");
         System.out.println("hello GitHub!");
+        System.out.println("hello GitHub2 hot-fix!");
     }
 }
