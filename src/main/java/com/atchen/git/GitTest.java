4aa70b9dcec6ce794b1969546c9ee0dde3969c80
zheming <455281092@qq.com>
2022-08-29 18:04:42 +0800

    Second commit

--- src/main/java/com/atchen/git/GitTest.java
+++ src/main/java/com/atchen/git/GitTest.java
@@ -10,6 +10,6 @@ public class GitTest {
         System.out.println("hello GitHub!");
         System.out.println("hello GitHub!");
         System.out.println("hello GitHub!");
-
+        System.out.println("hello GitHub!");
     }
 }
