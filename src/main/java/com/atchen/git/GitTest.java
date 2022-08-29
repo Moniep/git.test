0640dbef1aa9e78ec70a42746d79402781fceb2c
zheming <455281092@qq.com>
2022-08-29 18:03:51 +0800

    first commit

--- src/main/java/com/atchen/git/GitTest.java
+++ src/main/java/com/atchen/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello GitHub!");
         System.out.println("hello GitHub!");
+        System.out.println("hello GitHub!");
+
     }
 }
