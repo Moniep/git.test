18dcd822822becc9f80c9fefc739fed400415f3c
zheming <455281092@qq.com>
2022-08-29 17:32:44 +0800

    first commit

---
+++ src/main/java/com/atchen/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atchen.git;
+
+/**
+ * @author: zheming
+ * @data 2022/8/29 17:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
